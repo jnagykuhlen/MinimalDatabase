c7f6d2a0dd37ae54e2aefcd88bfd834bf645a04d
Jonas Nagy-Kuhlen <jonasnagykuhlen@aol.com>
2017-07-10 13:16:03 +0200

    Added unit test for testing storage system.

---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MinimalDatabase Unit Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MinimalDatabase Unit Tests")]
+[assembly: AssemblyCopyright("Copyright © 2017 Jonas Nagy-Kuhlen")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e0f29d3b-5f2a-4978-91a8-4d9d22f38181")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTests/StorageTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using MinimalDatabase;
+using MinimalDatabase.Internal;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class StorageTest
+    {
+        private const string FilePath = "TestDB.db";
+        private const uint StorageLength = 10000000;
+
+        private FilePersistenceService _persistenceService;
+        private PagingManager _pagingManager;
+        private StorageManager _storageManager;
+        private Stream _storageStream;
+        private uint _storagePageId;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _persistenceService = new FilePersistenceService(FilePath);
+            _pagingManager = new PagingManager(_persistenceService, Encoding.UTF8);
+            _storageManager = new StorageManager(_pagingManager);
+            _storagePageId = _storageManager.AllocateStorage(StorageLength);
+            _storageStream = _storageManager.GetStorageStream(_storagePageId);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _storageStream.Dispose();
+            _storageManager.DeallocateStorage(_storagePageId);
+            _persistenceService.Dispose();
+            File.Delete(FilePath);
+        }
+
+        [TestMethod]
+        public void TestRandomAccess()
+        {
+            Random random = new Random(42);
+            byte[] writeData = new byte[9247];
+            byte[] readData = new byte[writeData.Length];
+            int sampleLength = writeData.Length - 100;
+
+            random.NextBytes(writeData);
+
+            long[] samplePositions = new long[]
+            {
+                9956421,
+                 151345,
+                6635489,
+                 348913,
+                3448949,
+                1234565
+            };
+
+            random = new Random(42);
+            for (int i = 0; i < samplePositions.Length; ++i)
+            {
+                int writeOffset = random.Next(0, 100);
+                _storageStream.Position = samplePositions[i];
+                _storageStream.Write(writeData, writeOffset, sampleLength);
+            }
+
+            random = new Random(42);
+            for (int i = 0; i < samplePositions.Length; ++i)
+            {
+                _storageStream.Position = samplePositions[i];
+                int readBytes = _storageStream.Read(readData, i, sampleLength);
+                int writeOffset = random.Next(0, 100);
+
+                Assert.IsTrue(readBytes == sampleLength, "Less bytes read than written.");
+
+                bool isDataEqual = true;
+                for (int j = 0; j < sampleLength; ++j)
+                {
+                    if (readData[i + j] != writeData[writeOffset + j])
+                    {
+                        isDataEqual = false;
+                        break;
+                    }
+                }
+
+                Assert.IsTrue(isDataEqual, "Read data does not match written data.");
+            }
+        }
+    }
+}
